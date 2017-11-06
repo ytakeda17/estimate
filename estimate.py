8981c62b91dc68fa026c4183e9c1a12b8a98713c
ytakeda17 <puz.sf5@gmail.com>
2017-11-06 15:06:15 +0900

    tochu keika

--- estimate.py
+++ estimate.py
@@ -7,7 +7,21 @@ from scipy.stats import gmean
 def get_jsons(pics_dir, jsons_dir):
    pass
 
+def cosine(u,v):
+   #return min((1-abs(np.dot(u,v))/(np.linalg.norm(u)*np.linalg.norm(v)))*(2**(0.5)), 1)
+   return (1-abs(np.dot(u,v))/(np.linalg.norm(u)*np.linalg.norm(v)))*(2**(0.5))
 
+def top_mean(ary,threshold=0.2):
+   sorted_ary = sorted(ary,reverse=True)
+   selected = []
+   top = sorted_ary[0]
+   for element in sorted_ary:
+      if top-element < threshold:
+         selected.append(element)
+         top = element
+      else:
+         break
+   return np.mean(selected)
 
 if __name__=="__main__":
    argv = sys.argv
@@ -15,32 +29,55 @@ if __name__=="__main__":
    jsons_path = argv[2].rstrip("/")
    get_jsons(pics_path, jsons_path)
    jsons_path_format = jsons_path+"/*.json"
-   print(os.getcwd())
+   #print(os.getcwd())
+   res = {}
    for json_path in glob.glob(jsons_path_format):
-      score = 0
+      all_score = 0
       with open(json_path,"r") as f:
          data = json.load(f)
          people = data["people"]
-         rates = []
+         scores = []
+         heights = []
          for person in people:
-            pose = person['pose_keypoints']
+            score = 0
+            pose = np.array(person['pose_keypoints'])
+            ys = np.array([x for x in pose[list(range(1,54,3))] if x >0.01])
+            height = max(ys) - min(ys)
+            heights.append(height)
+            rsh = pose[2*3:3*3]
+            lsh = pose[5*3:6*3]
             rye = pose[14*3:15*3]
             lye = pose[15*3:16*3]
             rar = pose[16*3:17*3]
             lar = pose[17*3:18*3]
+
+            shoulder_line = rsh[:2]-lsh[:2]
+            l_line = lye[:2]-lar[:2]
+            r_line = rye[:2]-rar[:2]
+            
             possibilities = [x[2] for x in  [rye,lye,rar,lar]]
-            if np.max(possibilities)>0.5:
-               if len([x for x in possibilities if x > 0.1])== 4:
+            if max(possibilities)>0.5:
+               if min(possibilities) > 0.1:
                   rdis = ((rye[0]-rar[0])**2 + (rye[1]-rar[1])**2)**0.5
                   ldis = ((lye[0]-lar[0])**2 + (lye[1]-lar[1])**2)**0.5
-                  
-                  rates.append(np.min([rdis/ldis,ldis/rdis]))
+                  score = np.mean([cosine(l_line,shoulder_line),cosine(r_line,shoulder_line)])
+                  #rates.append(np.min([rdis/ldis,ldis/rdis]))
+                  score = np.min([rdis/ldis,ldis/rdis])
                else:
-                  rates.append(0)
-         if len(rates)>1:
+                  #rates.append(0)
+                  score = 1
+            else:
+               score = 0
+            scores.append(score*np.min([height/0.8,1]))
+         if len(scores)>0:
             #ave_rate = gmean(rates)
-            ave_rate = np.mean(rates)
+            #all_score = gmean(scores)#*max(heights)
+            #all_score = np.mean(scores)#*max(heights)
+            all_score = top_mean(scores)#*max(heights)
          else:
-            ave_rate = 1
-      score += 1-ave_rate
-      print([score, json_path])
+            all_score = 0
+      #print(scores)
+      #print(heights)
+      res[json_path.split("/")[-1]]=all_score
+      print([json_path.split("/")[-1],round(all_score*100)])
+   #print(sorted(res))
